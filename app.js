7c3807e3fca4356ff00a6014956e21aa76e18abf
Matthew Manzano <velarde.matthew@gmail.com>
2018-09-27 13:14:50 -0400

    app.js completed!

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 
-window.addEventListener('keydown', function(e) {
-  console.log(e.keyCode);
-});
+const keys = document.querySelectorAll('.key');
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+};
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound);
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
